ae91c21ad7532a056b50647907317ffe85107a55
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-11 02:56:39 +0530

    Initial Commit

---
+++ IAmRichUI/IAmRichUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichUIApp.swift
+//  IAmRichUI
+//
+//  Created by Usha Sai Chintha on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
