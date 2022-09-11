69d40cd273a75028d7f2d51fbe0f078dd985a5bf
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-11 12:49:14 +0530

    Initial Commit

---
+++ BusinessCardAppUI/BusinessCardAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardAppUIApp.swift
+//  BusinessCardAppUI
+//
+//  Created by Usha Sai Chintha on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
