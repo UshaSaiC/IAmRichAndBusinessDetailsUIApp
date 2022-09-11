1d70c9e4ea9331e9f4ffd12dbe4c270da93064f5
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-11 12:44:19 +0530

    Added swift UI code for designing a welcome/start screen

--- IAmRichUI/ContentView.swift
+++ IAmRichUI/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+// below struct defines how the user interface is going to look like and behave
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.systemPurple)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                .font(.system(size: 40))
+                // .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+                
+            }
+        }
     }
 }
 
+// below struct defines how the preview should be created and how it should behave
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 13"))
     }
 }
